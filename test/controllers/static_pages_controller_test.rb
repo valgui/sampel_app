a9be726d381e95d2c05d9e7440497dabea1a0bf6
Val Gui <valgui@gmail.com>
2018-02-07 15:45:08 -0800

    finished static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,6 +7,8 @@ def setup
   @base_title = "Ruby on Rails Tutorial Sample App"
 end
 
+
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -29,5 +31,10 @@ end
     get static_pages_contact_url
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
+  end
+
+  test "should get root" do
+  get root_url
+  assert_response :success
   end
 end
